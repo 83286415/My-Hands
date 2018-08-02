89cdad07b74347e46d3375576ac3f570964fe969
83286415 <83286415@163.com>
2018-08-02 17:02:23 +0800

    update 3 titanic source excel files

---
+++ transformation_pipelines/chapter4.py
@@ -0,0 +1,10 @@
+# To support both python 2 and python 3
+from __future__ import division, print_function, unicode_literals
+
+# Common imports
+import numpy as np
+import os
+
+# to make this notebook's output stable across runs
+np.random.seed(42)
+
