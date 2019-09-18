6c2d466537902f9087be331aca64845fa93d047e
83286415 <83286415@163.com>
2019-09-18 16:17:34 +0800

    Chapter 5 Linear SVM Classification - Non-Linear SVM Classification: Gaussian RBF Kernel

--- transformation_pipelines/chapter5_non_linear_classification.py
+++ transformation_pipelines/chapter5_non_linear_classification.py
@@ -70,6 +70,7 @@ def plot_dataset(X, y, axes):
     plt.ylabel(r"$x_2$", fontsize=20, rotation=0)
 
 
+# plot the hyperplane and contour line
 def plot_predictions(clf, axes):
     x0s = np.linspace(axes[0], axes[1], 100)
     x1s = np.linspace(axes[2], axes[3], 100)
@@ -240,4 +241,32 @@ if __name__ == '__main__':
     plt.subplots_adjust(right=0.2)  # adjust the distance between subplots
 
     save_fig("kernel_method_plot")
+    # plt.show()
+
+    # Gaussian RBF Kernel: SVC rbf kernel trick
+
+    # try different parameters combination like chapter 2 - grid search
+    gamma1, gamma2 = 0.1, 5
+    C1, C2 = 0.001, 1000
+    hyperparams = (gamma1, C1), (gamma1, C2), (gamma2, C1), (gamma2, C2)
+
+    svm_clfs = []  # to save different rbf_clf with different parameters
+    for gamma, C in hyperparams:
+        rbf_kernel_svm_clf = Pipeline([
+            ("scaler", StandardScaler()),
+            ("svm_clf", SVC(kernel="rbf", gamma=gamma, C=C))
+        ])
+        rbf_kernel_svm_clf.fit(X, y)
+        svm_clfs.append(rbf_kernel_svm_clf)
+
+    plt.figure(figsize=(11, 7))
+
+    for i, svm_clf in enumerate(svm_clfs):
+        plt.subplot(221 + i)  # 221, 222, 223, 224
+        plot_predictions(svm_clf, [-1.5, 2.5, -1, 1.5])  # plot the hyperplane and contour line
+        plot_dataset(X, y, [-1.5, 2.5, -1, 1.5])  # plot data set points: blue and green
+        gamma, C = hyperparams[i]
+        plt.title(r"$\gamma = {}, C = {}$".format(gamma, C), fontsize=16)
+
+    save_fig("moons_rbf_svc_plot")
     plt.show()
